cfcd3a88b76a8337911eb41afc418d550b38455f
iwatamasayuki <sorhoun3@gmail.com>
2021-01-25 16:36:17 +0900

    商品出品機能　改訂3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -74,14 +74,14 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'priceは、299円以下では出品できない' do
-        @item.price = @item.price < 299
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceは、10000000円以上では出品できない' do
-        @item.price = @item.price > 10_000_000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
     end
   end
