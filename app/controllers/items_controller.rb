cc6dc7a0a36c894d0a60a0bbd1ea9424ec8a394e
iwatamasayuki <sorhoun3@gmail.com>
2021-01-26 11:24:30 +0900

    商品情報編集機能実装　改訂3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
+    redirect_to action: :index unless current_user.id == @item.user.id
   end
 
   def update
